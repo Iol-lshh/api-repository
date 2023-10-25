5be3ca9f6d9483322d63cea4b136876bbba35bb7
lshh <markhong93@naver.com>
2023-10-25 16:00:24 +0900

    put extends service template

--- src/main/java/lshh/apirepository/service/api/request/ResourceRequestService.java
+++ src/main/java/lshh/apirepository/service/api/request/ResourceRequestService.java
@@ -4,9 +4,10 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import lshh.apirepository.dto.request.ResourceRequestDto;
+import lshh.apirepository.service.ServiceTemplate;
 
 @Service
-public interface ResourceRequestService {
+public interface ResourceRequestService extends ServiceTemplate {
 
     Optional<Object> getByPath(String path) throws Exception;
     Optional<Object> get(ResourceRequestDto dto) throws Exception;
