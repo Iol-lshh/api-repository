d75fce0c757cc5fa1f477a926c2b41c10dd49695
홍성혁 <luke0409@chunjae.co.kr>
2023-11-29 15:59:53 +0900

    catch error

--- back/src/main/java/lshh/apirepository/common/resourcer/ResourcerContext.java
+++ back/src/main/java/lshh/apirepository/common/resourcer/ResourcerContext.java
@@ -1,9 +1,15 @@
 package lshh.apirepository.common.resourcer;
 
+import java.util.List;
+import java.util.Map;
+
+import lshh.apirepository.dto.request.QueryMsgDto;
+
 public interface ResourcerContext{
     public enum Type{DB, API}
     int id();
     Type type();
     String name();
     long started();
+    List<Map<String, Object>> getResource(QueryMsgDto requestDto) throws Exception;
 }
